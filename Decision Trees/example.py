90f26c75b70e3588f3159ab11986734199ac8f95
Andrey Puzanov <puzanov.ee@gmail.com>
2024-11-08 06:42:56 +0300

    Add Decision Tree classifier

---
+++ Decision Trees/example.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from DecisionTrees import DecisionTreeClassifier
+
+
+# Dataset
+data = datasets.load_breast_cancer()
+X, y = data.data, data.target
+
+# Split the dataset into testing and training portions
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=14)
+
+# Classification model
+clf = DecisionTreeClassifier(max_depth=5)
+
+# Fit the model with the training data
+clf.fit(X_train, y_train)
+
+# Make predictions
+predictions = clf.predict(X_test)
+
+# Accuracy of the model
+acc = np.sum(predictions == y_test) / len(y_test)
+print(acc)
