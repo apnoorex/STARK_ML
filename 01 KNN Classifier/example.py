bacf14a7b899d6068ddf3df0fabd3c6679165daa
Andrey Puzanov <puzanov.ee@gmail.com>
2024-11-10 15:38:12 +0300

    Add KNN Classifier

---
+++ 01 KNN Classifier/example.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from KNeighborsClassifier import KNeighborsClassifier as KNN
+
+
+# Dataset
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+# Split the dataset into testing and training portions
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=51)
+
+# Model parameters
+n_neighbors = 17
+weight = 'distance' # {'uniform', 'distance'}
+
+# Classification model
+clf = KNN(n_neighbors, weights=weight)
+
+# Fit the model with the training data
+clf.fit(X_train, y_train)
+
+# Make predictions
+predictions = clf.predict(X_test)
+
+# Accuracy of the model
+acc = np.sum(predictions == y_test) / len(y_test)
+print(acc)
