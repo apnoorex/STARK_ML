92bc4bb4851c28b7b972b31eb514feed0fc9c167
Andrey Puzanov <puzanov.ee@gmail.com>
2024-11-16 13:29:04 +0300

    A typo

--- 10 KMeans/KMeans.py
+++ 10 KMeans/KMeans.py
@@ -24,7 +24,7 @@ class KMeans:
         consecutive iterations to declare convergence.
 
     random_state : int, default=None
-        Determines random number generator used to initializes the centroids.
+        Determines the random number generator used to initializes centroids.
     """
     def __init__(self, n_clusters=8, *, max_iter=300, tol=0.0001, random_state=None):
         # Parameters
